5e6d780b631534629852cd02659321808f9aa43c
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-27 05:56:40 -0700

    MAMAU : POST INCREMENTT POST DECREMENT SAB SMJ GAYA

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 
 // = k0 assign operator bhi khyty hain 
 
-var firstName = "zohaib"
-var lastName ="khan"
+//var firstName = "zohaib"
+//var lastName ="khan"
 
 // string topic 
 // + concatination  mean 2 string ko aps main jorny ko khty hain  e.g zohaib + khan = zohaib khan
@@ -42,26 +42,92 @@ var lastName ="khan"
 // console.log(num1num2)
 
 
-var num1 = +prompt('ENTER YOUR FIRST DIGIT')
-var num2 = prompt('ENTER YOUR FIRST DIGIT')
+//var num1 = +prompt('ENTER YOUR FIRST DIGIT')
+//var num2 = prompt('ENTER YOUR FIRST DIGIT')
 //  + wahid asa sign hai jisme agr pehli value bbina string ke hongi aur 2 value string ke sath hongi to vo concatinate kare ga yani joray ga !
 // prompt return karta hai string 
 
-console.log(num1+num2, typeof num1, typeof num2);
-document.write(num1+num2+"<br>")
-document.write(num1-num2+"<br>")
-document.write(num1/num2+"<br>")
-document.write(num1*num2+"<br>")
+// console.log(num1+num2, typeof num1, typeof num2);
+// document.write(num1+num2+"<br>")
+// document.write(num1-num2+"<br>")
+// document.write(num1/num2+"<br>")
+// document.write(num1*num2+"<br>")
 
 
-console.log(2+2);
+// console.log(2+2);
 
-console.log(2+(2*3-(3/4)));
-
-
+// console.log(2+(2*3-(3/4)));
 
 
+// 1+1 ++ --
+// increament or decreament topic 
+
+// // post increament
+// var numInc = 2
+// // numInc++
+// // console.log(numInc++);
+// console.log(++numInc);
+
+
+// var a = 2
+// var b = 3
+// var c = a++ + ++b - --a + ++b 
+// // 2+4-1+4
+// console.log(c);
+
+
+// postincrement a++ mean first print then add 
+// preincrement ++a mean first add then print 
 
 
 
+// var a = 2+1
+// ++a 
+// console.log(a++);
+// console.log(a);
 
+
+// for(var i = 0 ; i<=10;i++){
+//     document.write("danihs")
+// }
+
+// 1QUESTION SOLVE
+// var a =2 
+// var b = 1 
+// var result =  --a - --b + ++b + b--
+
+// console.log(result);
+// 1QUESTION SOLVE
+
+// 2QUESTION SOLVE
+// var a = 2
+// var b = 3
+// var c = 4
+
+// var d = a++ + a++ + ++a                       
+// 2       3+1
+// console.log(d);
+// 2QUESTION SOLVE
+
+// 3QUESTION SOLVE
+
+// var a = 2
+// var b = 3
+// var c = 4
+
+// var e = a++ + ++a + ++b + b + ++a - a++ - c-- - c-- - c++ + ++c + ++a 
+//                     console.log(e);                                    
+// console.log(e);
+// correct ans
+// 3QUESTION SOLVE
+
+
+// 4QUESTION SOLVE
+// var a = 2
+// var b = 3
+// var c = 4
+
+// var f = a++ - --c - c-- - b-- + --a + a++ - a++ - b++ + b++   - a++ + a++ + a 
+                                                      
+// console.log(f);
+// 4QUESTION SOLVE
