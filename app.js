a7be9809cce5dae751b8edaed7e62e89fca546d8
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-27 00:40:09 -0700

    variables ,string, number , assign opertor, initalization  and decleration string concatination,

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-var name 
+// data store in variable in string form 
+// var lastName;    // initialization mean varible ka name rakhna lakin uski value asssign nai ki 
+// lastName ="khan"
+//  decleration: jo variable initializ kia hai usko aik value assign karny ko decleration khty hain 
+
+// = k0 assign operator bhi khyty hain 
+
+var firstName = "zohaib"
+var lastName ="khan"
+
+// string topic 
+// + concatination  mean 2 string ko aps main jorny ko khty hain  e.g zohaib + khan = zohaib khan
+// + do number ky sath hoga tu wo math opertaion karyga e.g 2+2= 4
+
+
+// var fullName = firstName + " " + lastName
+// alert("my name is danish ")
+// alert(firstName)
+// document.write(firstName)
+// document.write(firstName)
+// document.write(firstName)
+// document.write(firstName)
+// document.write(firstName)
+// console.log(firstName)
+// console.log(firstName)
+// console.log(fullName)
+// firstName ="danish"
+// var job = true 
+// if(job){
+//     alert("fine")
+// }else{
+//     alert("not fine")
+// }
+
+
+// number 
+
+// var num1 = 2;
+// var num2 = 4;
+// // var num3 = ""
+// // var ans = prompt()
+// console.log(num1num2)
+
+
+var num1 = +prompt('ENTER YOUR FIRST DIGIT')
+var num2 = prompt('ENTER YOUR FIRST DIGIT')
+//  + wahid asa sign hai jisme agr pehli value bbina string ke hongi aur 2 value string ke sath hongi to vo concatinate kare ga yani joray ga !
+// prompt return karta hai string 
+
+console.log(num1+num2, typeof num1, typeof num2);
+document.write(num1+num2+"<br>")
+document.write(num1-num2+"<br>")
+document.write(num1/num2+"<br>")
+document.write(num1*num2+"<br>")
+
+
+console.log(2+2);
+
+console.log(2+(2*3-(3/4)));
+
+
+
+
+
+
+
+
